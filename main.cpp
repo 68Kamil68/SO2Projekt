a7a2dea3d6210bf768ac968f414aee9238ca231b
kamosin <kamosin@op.pl>
2020-05-24 22:08:09 +0200

    add player

--- main.cpp
+++ main.cpp
@@ -1,18 +1,38 @@
 #include <ncurses.h>
-#include <unistd.h>
+#include "player.h"
+#include <thread>
 using namespace std;
 
-int main(int argc, char ** argv) {
+
+int main(int argc, char ** argv)
+{
     initscr();
+
+    int h, w, startY, startX;
+    h = 25;
+    w = 90;
+    startY = startX = 5;
+    
+    WINDOW * win = newwin(h, w, startY, startX);
     refresh();
-    for(int i=0; i < 10; i++) {
-        mvprintw(i, i, "+");
-        refresh();
-        sleep(1);
-        mvdelch(i, i);
-    }
-    mvprintw(20, 20, "Hello");
-    getch();
+
+    box(win, 0, 0);
+    mvwprintw(win, 24, 35, "Falling asteroids game");
+    wrefresh(win);
+
+    Player * p =new Player(win, 15,15, '#');
+
+    
+    do
+    {
+        curs_set(0);
+        p->display();
+        wrefresh(win);
+    } while(p->getMove()!='q');
+    
+
+    //int c = getch();
+
     endwin();
     return 0;
 }
--- player.h
+++ player.h
@@ -1,12 +1,92 @@
 #include <tuple>
+#ifndef _PLAYER_H
+#define _PLAYER_H
+#endif
 
 class Player {
     public:
-        Player();
+        Player(WINDOW * win, int y, int x, char c);
         std::tuple<int, int> GetCoords();
-        void Move();
+        void MoveUp();
+        void MoveDown();
+        void MoveLeft();
+        void MoveRight();
+        void display();
+        int getMove();
 
     private:
-        int x, y;
-        
+        int xLoc, yLoc, xMax, yMax;
+        char character;
+        WINDOW * currentWindow;
 };
+
+    Player::Player(WINDOW * win, int y , int x, char c)
+    {
+        currentWindow = win;
+        yLoc = y;
+        xLoc = x;
+        getmaxyx(currentWindow, yMax, xMax);
+        keypad(currentWindow, true);
+        character = c;
+    }
+
+    void Player::MoveUp()
+    {
+        mvwaddch(currentWindow, yLoc, xLoc, ' ');
+        yLoc --;
+        if(yLoc <1)
+            yLoc =1;
+
+    }
+    
+    void Player::MoveDown()
+    {
+        mvwaddch(currentWindow, yLoc, xLoc, ' ');
+        yLoc++;
+        if(yLoc >yMax-2)
+            yLoc =yMax -2;
+    }
+
+    void Player::MoveLeft()
+    {
+        mvwaddch(currentWindow, yLoc, xLoc, ' ');
+        xLoc-=2;
+        if(xLoc < 1)
+            xLoc = 1;
+    }
+
+    void Player::MoveRight()
+    {
+        mvwaddch(currentWindow, yLoc, xLoc, ' ');
+        xLoc+=2;
+        if(xLoc > xMax-2)
+            xLoc = xMax-2;
+    }
+
+    int Player::getMove()
+    {
+        int choice = wgetch(currentWindow);
+        switch(choice)
+        {
+            case KEY_UP:
+                MoveUp();
+                break;
+            case KEY_DOWN:
+                MoveDown();
+                break;
+            case KEY_LEFT:
+                MoveLeft();
+                break;
+            case KEY_RIGHT:
+                MoveRight();
+                break;
+            default:
+                break;
+        }
+        return choice;
+    }
+
+    void Player::display()
+    {
+        mvwaddch(currentWindow, yLoc, xLoc, character);
+    }
